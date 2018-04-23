29a0abeaa5671feaaba5e8b082defba3748c3551
Alexander Verbruggen <alex@nabu.be>
2018-04-23 20:26:25 +0200

    updated

--- component/public/artifacts/views/table/table.js
+++ component/public/artifacts/views/table/table.js
@@ -25,12 +25,74 @@ nabu.views.dashboard.Table = Vue.extend({
 	data: function() {
 		return {
 			records: [],
-			paging: {}
+			paging: {},
+			configuring: false
 		}	
+	},
+	created: function() {
+		this.normalize(this.cell.state);
 	},
 	computed: {
 		operation: function() {
 			return this.cell.state.operation ? this.$services.swagger.operations[this.cell.state.operation] : null;
+		},
+		result: function() {
+			var properties = {};
+			if (this.operation) {
+				var schema = this.operation.responses["200"].schema;
+				var definition = this.$services.swagger.definition(schema["$ref"]);
+				if (definition.properties) {
+					var self = this;
+					Object.keys(definition.properties).map(function(field) {
+						if (definition.properties[field].type == "array") {
+							var items = definition.properties[field].items;
+							if (items["$ref"]) {
+								items = self.$services.swagger.definition(items["$ref"]);
+							}
+							if (items.properties) {
+								nabu.utils.objects.merge(properties, items.properties);
+							}
+						}
+					});
+				}
+			}
+			return properties;
+		},
+		// all the actual parameters (apart from the spec-based ones)
+		parameters: function() {
+			if (!this.operation) {
+				return [];
+			}
+			var blacklist = ["limit", "offset", "orderBy", "connectionId"];
+			var parameters = this.operation.parameters.filter(function(x) {
+				return blacklist.indexOf(x.name) < 0;
+			});
+			return parameters.map(function(x) { return x.name });
+		},
+		events: function() {
+			var result = {};
+			if (this.operation) {
+				var schema = this.operation.responses["200"].schema;
+				var definition = this.$services.swagger.definition(schema["$ref"]);
+				var parameters = [];
+				Object.keys(this.result).map(function(key) {
+					parameters.push(key);
+					// TODO: we have more metadata about the field here, might want to pass it along?
+				});
+				this.cell.state.actions.map(function(action) {
+					result[action.name] = parameters;
+				});
+			}
+			return result;
+		},
+		keys: function() {
+			return Object.keys(this.result);
+		},
+		orderable: function() {
+			// the operation must have an input parameter called "orderBy"
+			return this.operation && this.operation.parameters.filter(function(x) {
+				return x.name == "orderBy";
+			}).length > 0;
 		}
 	},
 	activate: function(done) {
@@ -39,6 +101,70 @@ nabu.views.dashboard.Table = Vue.extend({
 		});
 	},
 	methods: {
+		configure: function() {
+			this.configuring = true;	
+		},
+		normalize: function(state) {
+			if (!state.orderBy) {
+				Vue.set(state, "orderBy", []);
+			}
+			if (!state.limit) {
+				Vue.set(state, "limit", 20);
+			}
+			// actions you can perform on a single row
+			if (!state.actions) {
+				Vue.set(state, "actions", []);
+			}
+			
+			// we add a result entry for each field
+			// we can then set formatters for each field
+			if (!state.result) {
+				Vue.set(state, "result", {});
+			}
+			Object.keys(this.result).map(function(key) {
+				if (!state.result[key]) {
+					Vue.set(state.result, key, {
+						label: null,
+						format: null
+					});
+				}
+			});
+			var self = this;
+			this.parameters.map(function(x) {
+				if (!self.cell.bindings[x]) {
+					Vue.set(self.cell.bindings, x, null);
+				}
+			});
+		},
+		removeAction: function(action) {
+			var index = this.cell.state.actions.indexOf(action);
+			if (index >= 0) {
+				this.cell.state.actions.splice(index, 1);
+			}
+		},
+		addAction: function() {
+			this.cell.state.actions.push({
+				name: "unnamed",
+				icon: null
+			});
+		},
+		sort: function(key) {
+			if (this.orderable) {
+				var newOrderBy = [];
+				if (this.cell.state.orderBy.indexOf(key) >= 0) {
+					newOrderBy.push(key + " desc");
+				}
+				else if (this.cell.state.orderBy.indexOf(key + " desc") >= 0) {
+					// do nothing, we want to remove the filter
+				}
+				else {
+					newOrderBy.push(key);
+				}
+				this.cell.state.orderBy.splice(0, this.cell.state.orderBy.length);
+				nabu.utils.arrays.merge(this.cell.state.orderBy, newOrderBy);
+				this.load();
+			}
+		},
 		updateOperation: function(operation) {
 			this.cell.state.operation = operation.id;
 			var bindings = {};
@@ -79,11 +205,54 @@ nabu.views.dashboard.Table = Vue.extend({
 				return isAllowed;
 			});
 		},
+		// we want to be able to hide fields to reuse the same data source in multiple settings
+		isHidden: function(key) {
+			return this.cell.state.result[key] && this.cell.state.result[key].format == "hidden";	
+		},
+		interpret: function(key, value) {
+			if (value) {
+				var format = this.cell.state.result[key] ? this.cell.state.result[key].format : null;
+				if (format == "link") {
+					if (value.indexOf("http://") == 0 || value.indexOf("https://") == 0) {
+						return "<a target='_blank' href='" + value + "'>" + value.replace(/http[s]*:\/\/([^/]+).*/, "$1") + "</a>";
+					}
+				}
+				else if (format == "dateTime") {
+					value = new Date(value).toLocaleString();
+				}
+				else if (format == "date") {
+					value = new Date(value).toLocaleDateString();
+				}
+				else if (format == "time") {
+					value = new Date(value).toLocaleTimeString();
+				}
+			}
+			return value;
+		},
 		load: function(page) {
 			var promise = this.$services.q.defer();
 			if (this.cell.state.operation) {
 				var self = this;
-				this.$services.swagger.execute(this.cell.state.operation).then(function(list) {
+				var parameters = {};
+				if (this.orderable && this.cell.state.orderBy.length) {
+					parameters.orderBy = this.cell.state.orderBy;
+				}
+				
+				// we put a best effort limit & offset on there, but the operation might not support it
+				// at this point the parameter is simply ignored
+				var limit = this.cell.state.limit ? parseInt(this.cell.state.limit) : 20;
+				parameters.offset = (page ? page : 0) * limit;
+				parameters.limit = limit;
+				
+				var pageInstance = this.$services.page.instances[this.page.name];
+				// bind additional stuff from the page
+				Object.keys(this.cell.bindings).map(function(name) {
+					if (self.cell.bindings[name]) {
+						parameters[name] = pageInstance.get(self.cell.bindings[name]);
+					}
+				});
+				
+				this.$services.swagger.execute(this.cell.state.operation, parameters).then(function(list) {
 					self.records.splice(0, self.records.length);
 					Object.keys(list).map(function(field) {
 						if (list[field] instanceof Array && !self.records.length) {
@@ -102,6 +271,9 @@ nabu.views.dashboard.Table = Vue.extend({
 				promise.resolve("No operation found");
 			}
 			return promise;
+		},
+		configureParameters: function() {
+			
 		}
 	}
 });
--- component/public/artifacts/views/table/table.tpl
+++ component/public/artifacts/views/table/table.tpl
@@ -1,16 +1,53 @@
 <template id="dashboard-table">
 	<div>
-		<n-form class="layout2" v-if="edit">
-			<n-form-section class="operation">
-				<n-form-combo :value="operation" :filter="getOperations"
-					@input="updateOperation"
-					:formatter="function(x) { return x.id }"/>
-				<button @click="configureParameters">Configure</button>
-			</n-form-section>
-			<!-- TODO: add event management (with actions) -->
-		</n-form>
+		<n-sidebar @close="configuring = false" v-if="configuring">
+			<n-form-combo slot="header" :value="operation" :filter="getOperations"
+				@input="updateOperation"
+				:formatter="function(x) { return x.id }"/>
+			<n-form class="layout2">
+				<n-form-section class="misc">
+					<n-form-text v-model="cell.state.limit" label="Limit" :timeout="600" @input="load()"/>
+				</n-form-section>
+				<n-form-section class="mapping" v-if="parameters.length">
+					<h2>Input</h2>
+					<n-page-mapper :to="parameters" :from="$services.page.instances[page.name].availableParameters" 
+						v-model="cell.bindings"/>
+				</n-form-section>
+				<n-form-section class="actions">
+					<h2>Actions</h2>
+					<button @click="addAction">Add Action</button>
+					<n-form-section class="action" v-for="action in cell.state.actions">
+						<n-form-text v-model="action.name" label="Name" :required="true"/>
+						<n-form-text v-model="action.icon" label="Icon"/>
+						<button @click="removeAction(action)"><span class="n-icon n-icon-trash"></span></button>
+					</n-form-section>
+				</n-form-section>
+				<n-form-section class="formatters">
+					<h2>Formatting</h2>
+					<n-form-section class="formatter" v-for="key in keys">
+						<n-form-combo v-model="cell.state.result[key].format" :label="'Format ' + key + ' as'"
+							:items="['hidden', 'link', 'date', 'dateTime', 'time']"/>
+						<n-form-text v-model="cell.state.result[key].label" :label="'Label for ' + key" 
+							v-if="cell.state.result[key].format != 'hidden'"/>
+					</n-form-section>
+				</n-form-section>
+			</n-form>
+		</n-sidebar>
 		<table class="classic" cellspacing="0" cellpadding="0">
-			
+			<thead>
+				<tr>
+					<td @click="sort(key)" 
+							v-for="key in keys" v-if="!isHidden(key)"><span>{{ cell.state.result[key].label ? cell.state.result[key].label : key }}</span>
+						<span class="n-icon n-icon-sort-asc" v-if="cell.state.orderBy.indexOf(key) >= 0"></span>
+						<span class="n-icon n-icon-sort-desc" v-if="cell.state.orderBy.indexOf(key + ' desc') >= 0"></span>
+					</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="record in records">
+					<td v-for="key in keys" v-if="!isHidden(key)" :title="record[key]" v-html="interpret(key, record[key])"></td>
+				</tr>
+			</tbody>
 		</table>
 		<n-paging :value="paging.current" :total="paging.total" :load="load" :initialize="false"/>
 	</div>
